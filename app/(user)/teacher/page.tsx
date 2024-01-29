090041a0626ccfec2c6800fad357c3c92c0b2665
Daniel <chipperday@gmail.com>
2024-01-29 13:42:18 -0800

    ..

--- app/(user)/teacher/page.tsx
+++ app/(user)/teacher/page.tsx
@@ -1,3 +1,12 @@
-export default function TeacherHome() {
-  return <div>teacher!</div>
+import { Database } from "@/lib/database.types";
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+
+export default async function TeacherHome() {
+  const cookieStore = cookies();
+  const supabase = createServerComponentClient<Database>({
+    cookies: () => cookieStore,
+  });
+  const { data } = await supabase.from("profiles").select();
+  return <pre>from server: {JSON.stringify(data, null, 2)}</pre>;
 }
